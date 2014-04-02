c83c26a86daf6d7a254322c0ee1f6e63a82118ab
Atul Saurav <atul.saurav@gmail.com>
2014-04-02 07:09:34 -0500

    Initial Commit.. Able to read specfile

---
+++ MakeDelimited/MakeDelimited/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MakeDelimited
+//
+//  Created by Atul Saurav on 4/2/14.
+//  Copyright (c) 2014 Atul Saurav. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
